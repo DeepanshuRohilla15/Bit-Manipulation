be49c265f5f540432605111881afdda7dbe75609
DeepanshuRohilla15 <20001001027deepanshu@dcrustm.org>
2022-08-26 07:54:09 +0530

    Basic Function

---
+++ Xor_Calculator.cpp
@@ -0,0 +1,19 @@
+    // Method to calculate xor
+int computeXOR(int n)
+{
+   
+  // If n is a multiple of 4
+  if (n % 4 == 0)
+    return n;
+ 
+  // If n%4 gives remainder 1
+  if (n % 4 == 1)
+    return 1;
+ 
+  // If n%4 gives remainder 2
+  if (n % 4 == 2)
+    return n + 1;
+ 
+  // If n%4 gives remainder 3
+  return 0;
+}
